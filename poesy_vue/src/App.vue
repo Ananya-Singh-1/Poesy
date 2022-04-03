0b34dd5d8ef473e1ce258c46fc977058a3a3df33
Ananya Singh <ananyasingh121999@gmail.com>
2022-04-03 09:50:29 +0530

    Initial commit

---
+++ poesy_vue/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+ <div id="wrapper">
+   <nav class="navbar is-dark">
+     <div class="navbar-brand">
+       <router-link to="/" class="navbar-item"><strong>POESY</strong></router-link>
+
+       <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+         <span aria-hidden="true"></span>
+         <span aria-hidden="true"></span>
+         <span aria-hidden="true"></span>
+       </a>
+     </div>
+
+     <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+       <div class="navbar-end">
+         <router-link to="/frames" class="navbar-item">Frames</router-link>
+         <router-link to="/designs" class="navbar-item">Designs</router-link>
+         <router-link to="/poems" class="navbar-item">Poems</router-link>
+         <router-link to="/puns" class="navbar-item">Puns</router-link>
+          
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light is-rounded">Log in</router-link>
+
+              <router-link to="/cart" class="button is-success is-rounded">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+       </div>
+     </div>
+   </nav>
+
+   <section class="section">
+     <router-view/>
+   </section>
+
+   <footer class="footer">
+     <p class ="has-text-centered">Copyright (c) 2021</p>
+   </footer>  
+  </div>
+</template>
+
+<script>
+ export default {
+   data() {
+     return{
+       showMobileMenu: false,
+     }
+   }
+ }
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
